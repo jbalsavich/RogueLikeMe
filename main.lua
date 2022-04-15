b72d6a258c1bc067c2868565e80ac11f1712cc0c
jbalsavich <joebalsavich@gmail.com>
2022-04-15 00:36:31 -0500

    from 0,0,

--- main.lua
+++ main.lua
@@ -7,7 +7,20 @@ function love.load()
     
 end
 
+
+function ToInteger(number)
+    return math.floor(tonumber(number) or error("Could not cast '" .. tostring(number) .. "' to number.'"))
+end
+
+distance = 0
 function love.update(dt)
+
+    maxDistance = math.sqrt(math.pow(love.graphics.getWidth(), 2) + math.pow(love.graphics.getHeight(), 2))
+    distanceRatio = distance / maxDistance
+    realRed = math.floor(255 * distanceRatio)
+    backgroundcolor = {r= ToInteger(realRed), g= 0, b=255}
+    distance = math.sqrt(math.pow(player.x - 0, 2) + math.pow(player.y - 0, 2))
+    love.graphics.setBackgroundColor(ToInteger(backgroundcolor.r)/255,0,255)
     updateAll(dt)
     if love.keyboard.isDown('escape') then
         love.event.push('quit')
