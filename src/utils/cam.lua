ff7b32ecb9ecb7b8701da1292aca5b1f257d03c7
cjFREAK <stei1874@umn.edu>
2022-04-16 15:26:29 -0500

    added colliders and vector movement

--- src/utils/cam.lua
+++ src/utils/cam.lua
@@ -1,10 +1,34 @@
-Camera = require("libraries/plugins/hump-master/camera")
+Camera = require("libraries/plugins/hump/camera")
 cam = Camera()
 
 function cam:update(dt)
 
-    camX = player.x
-    camY = player.y
+    local camX, camY = player:getPosition()
+
+    local w = love.graphics.getWidth() / scale
+    local h = love.graphics.getHeight() / scale
+
+    local mapW = mapSize * spriteSize
+    local mapH = mapSize * spriteSize
+
+    -- Left border
+    if camX < w/2 then
+        camX = w/2
+    end
+
+    -- Right border
+    if camY < h/2 then
+        camY = h/2
+    end
+
+    -- Right border
+    if camX > (mapW - w/2) then
+        camX = (mapW - w/2)
+    end
+    -- Bottom border
+    if camY > (mapH - h/2) then
+        camY = (mapH - h/2)
+    end
 
     cam:lookAt(camX,camY)
     cam.x, cam.y = cam:position()
