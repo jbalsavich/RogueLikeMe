b25d6932686dcd3bc76a4118105653fc3f3048f6
jbalsavich <joebalsavich@gmail.com>
2022-04-17 23:20:27 -0500

    Revert to before 'Added Music'

--- src/player.lua
+++ src/player.lua
@@ -1,68 +1,77 @@
-player = {}
+player = world:newBSGRectangleCollider(190,160,12,15,3)
 player.x = 0
 player.y = 0
 player.width = love.graphics.getWidth()/15;
 player.height = love.graphics.getWidth()/15;
 player.dir = "down"
-player.speed = (player.width/love.graphics.getWidth()) * scale
+player.speed = (player.width/love.graphics.getWidth()) *100* scale
 
 
-player.health = 10
+player.health = 6
+
+
+-- 0 = Normal Gameplay
+player.state = 0
+
+player:setCollisionClass("Player")
+player:setFixedRotation(true)
 
 
 function player:update(dt)
-    if love.keyboard.isDown("right") then
-        player.dir = "right"
-        if player.x + player.width + player.speed >= love.graphics.getWidth() then
-            player.x = player.x - player.speed
-        end
-        player.x = player.x + player.speed
-    end
-    if love.keyboard.isDown("left") then
-        player.dir = "left"
-        if player.x - player.speed < 0 then
-            player.x = player.x + player.speed
-        end
-        player.x = player.x - player.speed
-    end
-    if love.keyboard.isDown("down") then
-        player.dir = "down"
-        if player.y + player.height + player.speed >= love.graphics.getHeight() then
-            player.y = player.y - player.speed
-        end
-        player.y = player.y + player.speed
-    end
-    if love.keyboard.isDown("up") then
-        player.dir = "up"
-        if player.y - player.speed < 0 then
-            player.y = player.y + player.speed
-        end
-        player.y = player.y - player.speed
-    end
-end
 
-function player:draw()
-    local plLf = love.graphics.newImage("sprites/playerLeft.png")
-    local plRt = love.graphics.newImage("sprites/playerRight.png")
-    local plUp = love.graphics.newImage("sprites/playerUp.png")
-    local plDw = love.graphics.newImage("sprites/playerDown.png")
+    local dirX = 0
+    local dirY = 0
 
+    if player.state == 0 then
+        if love.keyboard.isDown("right") then
+            player.dir = "right"
+            dirX = 1
+        end
+        if love.keyboard.isDown("left") then
+            player.dir = "left"
+            dirX = -1
+        end
+        if love.keyboard.isDown("down") then
+            player.dir = "down"
+            dirY = 1
+        end
+        if love.keyboard.isDown("up") then
+            player.dir = "up"
+            dirY = -1
+        end
 
-    if player.dir == "right" then
-        love.graphics.draw(plRt, player.x, player.y, 0,nil,nil,plRt:getWidth()/2, plRt:getHeight()/2)
-    
-    elseif player.dir == "left" then
-        love.graphics.draw(plLf, player.x, player.y, 0,nil,nil,plLf:getWidth()/2, plLf:getHeight()/2)
+        local vec = vector(dirX,dirY):normalized() * player.speed
+        player:setLinearVelocity(vec.x,vec.y)
 
-    elseif player.dir == "down" then
-        love.graphics.draw(plDw, player.x, player.y, 0,nil,nil,plDw:getWidth()/2, plDw:getHeight()/2)
-    
-    elseif player.dir == "up" then
-        love.graphics.draw(plUp, player.x, player.y, 0,nil,nil,plUp:getWidth()/2, plUp:getHeight()/2)
         
     end
 end
 
+function player:draw()
+
+    local plLf = sprites.playerLeft
+    local plRt = sprites.playerRight
+    local plUp = sprites.playerUp
+    local plDw = sprites.playerDown
+
+    local px = player:getX()+1
+    local py = player:getY()-5
+
+
+    if player.dir == "right" then
+        love.graphics.draw(plRt, px, py, 0,nil,nil,plRt:getWidth()/2, plRt:getHeight()/2)
+    
+    elseif player.dir == "left" then
+        love.graphics.draw(plLf, px, py, 0,nil,nil,plLf:getWidth()/2, plLf:getHeight()/2)
+
+    elseif player.dir == "down" then
+        love.graphics.draw(plDw, px, py, 0,nil,nil,plDw:getWidth()/2, plDw:getHeight()/2)
+    
+    elseif player.dir == "up" then
+        love.graphics.draw(plUp, px, py, 0,nil,nil,plUp:getWidth()/2, plUp:getHeight()/2)
+        
+    end
+end
 
 --[[
     function love.draw(dt)
